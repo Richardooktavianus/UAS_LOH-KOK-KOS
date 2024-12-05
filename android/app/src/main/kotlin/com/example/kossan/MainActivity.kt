25727dc9ccb896fd6a8e3977e9e39e5368b433ee
Richardooktavianus <zeusmati1234@gmail.com>
2024-12-05 21:00:42 +0700

    firebase tahap 1

--- android/app/src/main/kotlin/com/example/kossan/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kossan/MainActivity.kt
@@ -1,5 +1,4 @@
-package com.example.kossan
-
+package com.lkk.app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kossan/firebase_options.dart';
 import 'package:kossan/provider/booking_provider.dart';
 import 'package:kossan/provider/favorites_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:kossan/provider/theme_provider.dart';
 import 'package:kossan/screen/splash_screen.dart';
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
